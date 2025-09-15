1392e3469900d8ee4c6be5abf6ee69da837f4325
student <rahoust@miners.utep.edu>
2025-09-14 20:31:45 -0600

    added new font (8x12)

--- draw_chars.c
+++ draw_chars.c
@@ -14,3 +14,17 @@ void print_char_5x7(char c)
   }
   putchar('\n');
 }
+
+void print_char_8x12(char c) {
+  unsigned int code = (unsigned char)c;
+  if (code < 0x20 || code > 0x7E) code = 0x20;   // clamp to printable
+  code -= 0x20;
+  for (int row = 0; row < 12; row++) {
+    unsigned char bits = font_8x12[code][row]; // 8 bits = 8 columns
+    for (int col = 7; col >= 0; col--) {
+      putchar((bits & (1u << col)) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+  putchar('\n');
+}
